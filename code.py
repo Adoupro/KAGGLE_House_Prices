8438fa55784bfcd5a4b9725239e440d0103b3c03
Adoupro <adou.professionnel@gmail.com>
2018-04-05 10:02:10 +0200

    Version 2

--- code.py
+++ code.py
@@ -9,6 +9,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -18,8 +20,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 from sklearn.ensemble import RandomForestRegressor
-
-
 
 dataset = pd.read_csv('train.csv', index_col = 'Id')
 kaggle = pd.read_csv('test.csv', index_col = 'Id')
@@ -56,6 +56,11 @@ def data_cleaning(data):
         data[column] = data[column].fillna('Empty')
         data[column] = encoder.fit_transform(data[column].astype(str))
     
+    # Optimisation
+    
+    cols = []
+    data = data.drop(cols, axis = 1)
+    
     return data
 
 
@@ -68,11 +73,27 @@ def features_labels_split(data, y_col):
 
 def best_features(data, y_col, limit):
     correlation = np.abs(data.corr())
+    
+    # In the purpose to localise not usefull features
+    test = correlation
+    test = test.sort_values(y_col, ascending = False)
+    mask = correlation[y_col] >= limit
+    test = test[mask]
+    cols = test.index.values
+    test = test[cols] >= limit
+    
     correlation = correlation[y_col].sort_values(ascending = False)
     mask = correlation > limit
     correlation = correlation[mask]
     result = correlation.index.values[1:]
     return result
+
+
+def baseline(name, model):
+    model.fit(X_train, y_train)
+    complexity_score = model.score(X_train, y_train)
+    generalisation_score = model.score(X_test, y_test)
+    print('%s\n\nComplexity : %s.\nGeneralisation : %s.\n\n' % (name, complexity_score, generalisation_score))
 
 
 def top_parameters(name, model, params):
@@ -90,14 +111,7 @@ def top_parameters(name, model, params):
     generalisation_score = grid.best_score_
     print('For parameters : %s.' % parameters)
     print('Complexity : %s. \nGeneralisation : %s.\n\n' % (complexity_score, generalisation_score))
-
-
-def baseline(name, model):
-    model.fit(X_train, y_train)
-    complexity_score = model.score(X_train, y_train)
-    generalisation_score = model.score(X_test, y_test)
-    print('%s\n\nComplexity : %s.\nGeneralisation : %s.\n\n' % (name, complexity_score, generalisation_score))
-
+    
 
 # =============================================================================
 # Project
@@ -107,6 +121,7 @@ def baseline(name, model):
     
 dataset = data_cleaning(dataset)
 
+
 # Selection of the best features (Minimum 0.5 or -0.5 of correlation with the 'SalePrice' column)
 
 X_cols = best_features(dataset, 'SalePrice', 0.5)
@@ -115,12 +130,18 @@ X_cols = best_features(dataset, 'SalePrice', 0.5)
 # Separation of X and y
 
 X, y = features_labels_split(dataset, 'SalePrice')
+
 X = X[X_cols]
 
 
 # Split X_train, X_test, y_train, y_test 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
 
 
 # Optimisation 
@@ -132,7 +153,6 @@ n_model = len(names)
 for i in range(n_model):
     baseline(names[i], models[i])
     
-
 
 names = ['Ridge Regression', 'Lasso Regression', 'Random Forest Regressor']
 models = [Ridge(), Lasso(), RandomForestRegressor()]
@@ -154,10 +174,14 @@ for i in range(n_model):
 kaggle = data_cleaning(kaggle)
 kaggle = kaggle[X_cols]
 
-
-model = RandomForestRegressor(max_depth = 10, max_features = 4, n_estimators = 10)
+model = RandomForestRegressor(max_depth = 10, max_features = 6, n_estimators = 9)
 model.fit(X_train, y_train)
-prediction = pd.Series(model.predict(kaggle), index = kaggle.index.values)
+
+Id = kaggle.index.values
+SalePrice = model.predict(kaggle)
+
+prediction = pd.DataFrame({'Id' : Id, 'SalePrice' : SalePrice})
+prediction = prediction.set_index('Id')
 prediction.to_csv('result.csv')
 
 print(prediction)
